39a454eb005f8a5cda258b323b7f5ca0e98ce06e
jacquelineleone <jacqueline.leone02@gmail.com>
2024-11-05 23:29:32 -0300

    feat: create Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import "./Button.css";
+
+interface Props {
+  children: ReactNode
+  parentMethod: () => void;
+}
+
+export const Button = ({ children, parentMethod }: Props) => {
+
+  return (
+    <button className="custom-button" onClick={parentMethod}>
+      {children}
+    </button>
+  );
+};
