6459e20b1a3bd174f9e709f0721b42dfbdd56c44
Jacqueline <jacqueline.leone02@gmail.com>
2024-11-06 13:28:15 -0300

    feat: setup form context for data handling

--- src/context/useForm.tsx
+++ src/context/useForm.tsx
@@ -1,9 +1,22 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 
+interface FormData {
+  name: string;
+}
+
+const defaultFormData: FormData = {
+  name: "",
+};
+
 interface FormContextType {
   currentStep: number;
   nextStep: () => void;
   previousStep: () => void;
+  formData: FormData;
+  handleChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  setFormData: (data: FormData) => void;
 }
 
 interface FormProps {
@@ -25,6 +38,7 @@ export const useFormContext = () => {
 
 export const FormProvider = ({ children, totalSteps }: FormProps) => {
   const [currentStep, setCurrentStep] = useState<number>(0);
+  const [formData, setFormData] = useState<FormData>(defaultFormData);
 
   const nextStep = () =>
     setCurrentStep((prevStep) => Math.min(prevStep + 1, totalSteps - 1));
@@ -32,8 +46,24 @@ export const FormProvider = ({ children, totalSteps }: FormProps) => {
   const previousStep = () =>
     setCurrentStep((prevStep) => Math.max(prevStep - 1, 0));
 
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
   return (
-    <FormContext.Provider value={{ currentStep, nextStep, previousStep }}>
+    <FormContext.Provider
+      value={{
+        currentStep,
+        nextStep,
+        previousStep,
+        formData,
+        handleChange,
+        setFormData,
+      }}
+    >
       {children}
     </FormContext.Provider>
   );
