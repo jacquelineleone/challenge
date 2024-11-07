a9a752125bc738726ca0dc97cb3826b41fa3ca6e
Jacqueline <jacqueline.leone02@gmail.com>
2024-11-07 10:08:48 -0300

    feat: manage disabled next button in NavigationButtons

--- src/components/Buttons/NavigationButtons/NavigationButtons.tsx
+++ src/components/Buttons/NavigationButtons/NavigationButtons.tsx
@@ -4,26 +4,30 @@ import backArrow from "../../../assets/images/back-arrow.svg";
 import styles from "./styles.module.css";
 
 export const NavigationButtons = () => {
-  const { currentStep, totalSteps, nextStep, previousStep } = useFormContext();
+  const { currentStep, totalSteps, nextStep, previousStep, errors } =
+    useFormContext();
 
   const getNextButtonText = () => {
     if (currentStep === 0) return "Comenzar";
     return "Siguiente";
   };
 
-  const isPrevButtonVisible = () => currentStep > 0 && currentStep < totalSteps - 1;
+  const isPrevButtonVisible = () =>
+    currentStep > 0 && currentStep < totalSteps - 1;
 
   return (
     <div className={styles.buttons}>
       {isPrevButtonVisible() && (
-        <Button
-          parentMethod={previousStep}
-          className={styles.previousButton}
-        >
+        <Button parentMethod={previousStep} className={styles.previousButton}>
           <img src={backArrow} alt="volver" loading="lazy" />
         </Button>
       )}
-      <Button parentMethod={nextStep} className={styles.nextButton}>
+      <Button
+        parentMethod={nextStep}
+        className={`${styles.nextButton} ${
+          Object.keys(errors).length === 0 ? styles.enabled : styles.disabled
+        }`}
+      >
         <p>{getNextButtonText()}</p>
       </Button>
     </div>
