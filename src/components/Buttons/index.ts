7363d3d8fb5dd104407f98a22c02c61a1f79c1a9
jacquelineleone <jacqueline.leone02@gmail.com>
2024-11-07 02:30:31 -0300

    chore: update buttons barrel

--- src/components/Buttons/index.ts
+++ src/components/Buttons/index.ts
@@ -1,2 +1,3 @@
 export * from './Button';
-export * from './NavigationButtons/NavigationButtons'
+export * from './NavigationButtons/NavigationButtons';
+export * from './SubmitButton/SubmitButton';
